ed68aee33046851188cc95e75ffeef2fc7854122
HotDog640 <Hotdog640derp21@gmail.com>
2022-02-28 12:18:45 +0000

    WIP

---
+++ Bookwork/Chapter 3/p84 Tasks.py
@@ -0,0 +1,42 @@
+# p84 Tasks
+# Q1
+"""
+ages = [32, 31, 30, 37, 37, 33]
+earnings = [1270, 7980, 4700, 1810, 750, 1100]
+
+minAge = min(ages)
+maxAge = max(ages)
+minEarning = min(earnings)
+maxEarning = max(earnings)
+
+print(f"minAge: {minAge}")
+print(f"maxAge: {maxAge}")
+print(f"minEarning: {minEarning}")
+print(f"maxEarning: {maxEarning}")
+"""
+
+# Q2
+
+from statustics import mean, median, mode
+list = [1, 9, 3, 2, 3, 7, 6, 5, 8, 9, 1, 10]
+minE = min(list)
+maxE = max(list)
+meanE = mean(list)
+medianE = median(list)
+modeE = mode(list)
+
+# frequency
+items = []
+frequencies = []
+
+for item in list:
+    if item not in names:
+        names.append(item)
+for item in items:
+    total = list.count(item)
+    frequencies.append(total)
+    
+print(
+"""
+
+""") 

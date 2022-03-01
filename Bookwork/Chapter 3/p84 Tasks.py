fad2ace75dfd756855cd9344e9c5eff0a2211e5b
HotDog640 <Hotdog640derp21@gmail.com>
2022-03-01 10:18:32 +0000

    wip

--- Bookwork/Chapter 3/p84 Tasks.py
+++ Bookwork/Chapter 3/p84 Tasks.py
@@ -16,27 +16,66 @@ print(f"maxEarning: {maxEarning}")
 """
 
 # Q2
-
-from statustics import mean, median, mode
+"""
+from statistics import mean, median, mode
 list = [1, 9, 3, 2, 3, 7, 6, 5, 8, 9, 1, 10]
 minE = min(list)
 maxE = max(list)
 meanE = mean(list)
 medianE = median(list)
-modeE = mode(list)
 
 # frequency
 items = []
 frequencies = []
 
 for item in list:
-    if item not in names:
-        names.append(item)
+    if item not in items:
+        items.append(item)
 for item in items:
     total = list.count(item)
     frequencies.append(total)
     
+# mode
+mostOccurences = max(frequencies)
+index = frequencies.index(mostOccurences)
+mode = items[index]
+
+    
 print(
+    f"min: {minE}\n" +
+    f"max: {maxE}\n" +
+    f"mean: {meanE}\n" +
+    f"median: {medianE}\n" +
+    f"mode: {mode}\n" +
+    "\n" +
+    f"frequency:\n" +
+    f"{items}\n" +
+    f"{frequencies}"
+)
 """
 
-""") 
+# Q3
+# lottery.ie is blocked
+
+# Q4
+from statistics import mode
+federalist = open("hamilton-federalist-548.txt", "r").read()
+words = federalist.split()
+
+wordlist = []
+frequency = []
+
+for word in words:
+    if word not in wordlist:
+        wordlist.append(word)
+for word in wordlist:
+    total = words.count(word)
+    frequency.append(total)
+
+print(wordlist)
+print(frequency)
+
+
+
+
+

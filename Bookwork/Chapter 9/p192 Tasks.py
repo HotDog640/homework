1c1db5da6cead24625e48295cda1dbb1f8012eb1
HotDog640 <Hotdog640derp21@gmail.com>
2022-04-28 13:45:17 +0100

    Chapter 9

--- Bookwork/Chapter 9/p192 Tasks.py
+++ Bookwork/Chapter 9/p192 Tasks.py
@@ -16,8 +16,33 @@
 """
 
 # Q2
-
+"""
 for timesTable in range(1, 13):
     print(f"\n\nTimes table for {timesTable}:\n")
     for i in range(1, 13):
         print(timesTable, "x", i, "=", timesTable * i)
+"""
+
+# Q3
+"""
+rows = int(input("How many rows do you want? "))
+cols = int(input("How many columns do you want? "))
+
+for row in range(rows):
+    for col in range(cols):
+        print("*", end = "")
+    print()
+"""
+
+# Q4
+"""
+rows = int(input("How many rows do you want? "))
+for row in range(rows+1):
+    for i in range(row):
+        print("*", end="")
+    print()
+"""
+
+# Q5
+
+rows = int(input("How many rows do you want? "))
---
+++ Bookwork/Chapter 9/p203 Tasks.py
@@ -0,0 +1,93 @@
+# p203 Tasks
+
+# Q1
+"""
+def getPi():
+    return 22/7
+
+print(getPi())
+"""
+
+# Q2
+"""
+def divide(x, y):
+    return x/y
+
+print(divide(6, 3))
+"""
+
+# Q3
+"""
+def spaceCount(string):
+    return string.count(" ")
+
+print(spaceCount("The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues."))
+"""
+
+# Q4
+"""
+def dividesEvenly(x, y):
+    return x % y == 0
+
+print(dividesEvenly(64, 8))
+"""
+
+# Q5
+"""
+def occurs(theList, value):
+    return theList.count(value)
+
+print(occurs([1, 2, 4, 2, 2, 2], 2))
+"""
+
+# Q6
+"""
+def getSalesPrice(RPR, discount):
+    return RPR - ((discount / 100) * RPR)
+
+RPRs = [23, 41, 3, 76, 235, 3]
+discounts = [64, 21, 5, 32, 64, 50]
+
+print("Retail\t% Off\tSales Price")
+for RPR, discount in zip(RPRs, discounts):
+    salesPrice = getSalesPrice(RPR, discount)
+    print(f"{RPR}\t{discount}\t{salesPrice}")
+"""
+
+# Q7
+"""
+def convertTempFrom(temp, scale):
+    if scale == "C":
+        return (temp * 1.8) + 32
+    elif scale == "F":
+        return (temp - 32) / 1.8
+
+temps = [50, 102, 37, 20]
+scales = ["C", "F", "C", "F"]
+for temp, scale in zip(temps, scales):
+    otherTemp = convertTempFrom(temp, scale)
+    otherScale = "F" if scale == "C" else "C"
+    print(f"{temp}°{scale} = {otherTemp:.2f}°{otherScale}")
+"""
+
+# Q8
+
+# returns float of the price in euro
+def getTheCostOfPetFoodForThreeWeeks(petType, gramsPerDay):
+    foodPrice = None
+    if petType == "cat":
+        foodPrice = 5
+    elif petType == "hamster":
+        foodPrice = 3
+        
+    kilosPerDay = gramsPerDay/1000
+    
+    return (foodPrice * 21 * kilosPerDay)
+
+petType = input("What type of pet do you have? ")
+gramsPerDay = int(input("How much grams of food do they eat a day? "))
+
+costOfPetFoodForThreeWeeks = getTheCostOfPetFoodForThreeWeeks(petType, gramsPerDay)
+
+print(f"The cost of pet food for the next three weeks is: €{costOfPetFoodForThreeWeeks:.2f}")
+
